9e92d75450dc2a52bde2f33c2ed371dd74439cbc
vijayporwal12345 <118162779+vijayporwal12345@users.noreply.github.com>
2025-09-24 19:05:55 +0530

    get users details

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get('/api/get', (req,res) => {
 res.send({message:"hello back to node js"});
 })
 
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"satyam",
+            age:22,
+            contact:12254
+        },env:process.env.NAME
+    })
+});
+
+
 app.listen(8000,() => {
 console.log("listing to 8000");
 
