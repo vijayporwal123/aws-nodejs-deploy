7f642ecc0a478e5bc28be27d9607b423646bbb1a
vijayporwal12345 <118162779+vijayporwal12345@users.noreply.github.com>
2025-09-26 18:51:46 +0530

    added .env

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express= require("express");
 const app = express();
+require("dotenv").config
 
 
 app.get('/api/get', (req,res) => {
@@ -17,7 +18,8 @@ app.get('/api/get_user_details',(req,res) => {
 });
 
 
-app.listen(8000,() => {
+//app.listen(8000,() => {
+ app.listen(process.env.PORT,() => {   
 console.log("listing to 8000");
 
 
